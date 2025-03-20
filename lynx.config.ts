7e67cdf18dd30c2f7d2dd8c413d997de62641035
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-03-20 10:09:36 +0530

    Refactor code formatting and improve indentation; update comments for clarity

--- lynx.config.ts
+++ lynx.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     pluginQRCode({
       schema(url) {
-        // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
         return `${url}?fullscreen=true`
       },
     }),
--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ import { App } from './App.js'
 root.render(<App />)
 
 if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
+    import.meta.webpackHot.accept()
 }
